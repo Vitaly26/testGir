d5fefe6b90830af44238f83226f4a2e16ffe6786
Виталий <pavelivanov@MacBook-Pavel-Ivanov.local>
2012-01-23 19:03:31 +0400

    Initial Commit

---
+++ t1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  t1
+//
+//  Created by Павел Иванов on 23.01.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
