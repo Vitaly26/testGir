b03b57a2137b6881d48574a61fb9895b22b4df94
Vitaly Pastushin <vitalik2005@mail.ru>
2012-01-23 22:08:05 +0400

    new class

---
+++ new.h
@@ -0,0 +1,13 @@
+//
+//  new.h
+//  t1
+//
+//  Created by Павел Иванов on 23.01.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface new : NSObject
+
+@end
---
+++ new.m
@@ -0,0 +1,13 @@
+//
+//  new.m
+//  t1
+//
+//  Created by Павел Иванов on 23.01.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "new.h"
+
+@implementation new
+
+@end
